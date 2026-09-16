1824335e61b21910b338f145face0223f42392aa
WoodrowtheFox <thnlundgren@gmail.com>
2026-09-16 15:51:19 -0500

    Need some more tweaks

--- EmailHeaderAnalyzer.py
+++ EmailHeaderAnalyzer.py
@@ -1,3 +1,6 @@
+import urllib.request
+import json
+
 def readfile(filename):
     data = {}
     file = open(filename, "r");
@@ -8,6 +11,7 @@ def readfile(filename):
         currline = [word.replace(")", "") for word in currline]
         currline = [word.replace("[", "") for word in currline]
         currline = [word.replace("]", "") for word in currline]
+        currline = [word.replace(",", "") for word in currline]
         data[i] = currline;
         i += 1;
     data[i] = "THE FILE IS OVER NOW"
@@ -18,19 +22,30 @@ def extractdetails(datafromfile):
     values = {}
     i = 0;
     while(datafromfile.get(i) != "THE FILE IS OVER NOW"):
-        if(values.get("Server") in datafromfile.get(i)):
-            values["IP"] = datafromfile.get(i)[1]
-        if("from" in datafromfile.get(i)):
-            values["Server"] = datafromfile.get(i)[2] + "."
-        if("by" in datafromfile.get(i) and len(datafromfile.get(i)) == 2):
-            values["Destination"] = datafromfile.get(i)[1]
-        if("with" in datafromfile.get(i) and len(datafromfile.get(i)) == 4):
-            values["Protocol"] = datafromfile.get(i)[1]
-        if("with" in datafromfile.get(i) and len(datafromfile.get(i)) == 4):
-            values["Protocol"] = datafromfile.get(i)[1]
+        list = datafromfile.get(i);
+        if(values.get("Server")):
+            values["IP"] = list[list.index(values.get("Server")) + 1]
+        if("from" in list):
+            values["Server"] = list[list.index("from") + 1] + "."
+        if("by" in list):
+            values["Destination"] = list[list.index("by") + 1]
+        if("with" in list):
+            values["Protocol"] = list[list.index("with") + 1]
+        if(("Mon" in list) or ("Tue" in list) or ("Wed" in list) or ("Thu" in list) or ("Fri" in list) or ("Sat" in list) or ("Sun" in list)):
+            days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+            for day in days:
+                if(day in list):
+                    values["Timestamp"] = list[list.index(day)]+ " " + list[list.index(day) + 1] + " " + list[list.index(day) + 2] + " " + list[list.index(day) + 3] + " " + list[list.index(day) + 4]
         i += 1;
-    values["TimeStamp"] = datafromfile.get(14)[0]+ " " + datafromfile.get(14)[1] + " " + datafromfile.get(14)[2] + " " + datafromfile.get(14)[3] + " " + datafromfile.get(14)[4]
     return values;
+
+def check_ip(ip):
+    url = f"http://ip-api.com/json/{ip}"
+    with urllib.request.urlopen(url) as response:
+        return json.loads(response.read())
+
+    info = check_ip("8.8.8.8")
+    print(info["city"], info["country"], info["isp"])
 
 def main():
     val = input("Please enter the file with the email header:\n");
@@ -38,4 +53,5 @@ def main():
     values = extractdetails(data);
     for key in values:
         print(key + ": " + values.get(key) + "\n")
+    check_ip(values.get("IP"));
 main();
