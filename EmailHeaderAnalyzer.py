8d0a3bb3e170a6a014146abcb36cd772c14c5ad0
WoodrowtheFox <thnlundgren@gmail.com>
2026-09-16 13:30:25 -0500

    Working on extracting data from the file read

--- EmailHeaderAnalyzer.py
+++ EmailHeaderAnalyzer.py
@@ -1,34 +1,23 @@
-def readfile(input):
-    with open("Email.txt", "w") as f:
-        f.write(input)
-    data = {};
-    file = open("Email.txt", "r");
+def readfile(filename):
+    data = {}
+    file = open(filename, "r");
     i = 0;
     for line in file:
-        data[i] = line;
+        currline = line.split();
+        data[i] = currline;
         i += 1;
     file.close();
     return data;
 
+def extractdetails(datafromfile):
+    values = {}
+    i = 0;
+    while(datafromfile.get(i)):
+        print(datafromfile.get(i))
+        i += 1;
+
 def main():
-    print("Please enter the filepath\n");
-    data = readfile("Delivered-To: haiyang@d.umn.edu Received: by 2002:a05:6402:1907:0:0:0:0 with SMTP id e7csp4086589edzMon, 1 Feb 2021 08:44:25 -0800 (PST)" +
-                    "X-Received: by 2002:a05:6638:1344:: with SMTP id u4mr15832375jad.86.1612197865113;" +
-                    "Mon, 01 Feb 2021 08:44:25 -0800 (PST)" +
-                    "Return-Path: <cox00133@d.umn.edu>" +
-                    "Received: from mta-p8.oit.umn.edu" +
-                    "(mta-p8.oit.umn.edu. [134.84.196.208])" +
-                    "by mx.google.com" +
-                    "with ESMTPS id o9si10466400ioo.93.2021.02.01.08.44.24" +
-                    "for <haiyang@d.umn.edu>;" +
-                    "Mon, 01 Feb 2021 08:44:25 -0800 (PST)" +
-                    "Received-SPF: pass" +
-                    "(google.com: domain of cox00133@d.umn.edu" +
-                    "designates 209.85.166.70 as permitted sender)" +
-                    "client-ip=209.85.166.70;" +
-                    "Authentication-Results: mx.google.com;" +
-                    "dkim=pass;" +
-                    "spf=pass);" +
-                    "for i in data: " +
-                    "print(data[i]);");
+    val = input("Please enter the file with the email header");
+    data = readfile(val);
+    extractdetails(data);
 main();
